a34cd1d65ff254bd2de30cbefd87460cccccd2b0
Akshay Deshraj <akshay30nov@gmail.com>
2015-02-07 13:17:40 +0530

    Remove unnecessary functions

--- server/server.py
+++ server/server.py
@@ -5,15 +5,18 @@ app = Flask(__name__)
 def api_root():
 	return 'Welcome'
 
-@app.route('/toggle')
-def api_toggle():
-   # Toggle the state of the player
-   return 'state changed'    
+# Deprecated
 
-@app.route('/volume/<volume_value>')
-def api_volume(volume_value):
-	# Adjusts volume of the player
-	return 'Volume is now ' + volume_value
+# @app.route('/toggle')
+# def api_toggle():
+#    # Toggle the state of the player
+#    return 'state changed'    
+
+# @app.route('/volume/<volume_value>')
+# def api_volume(volume_value):
+# 	# Adjusts volume of the player
+# 	return 'Volume is now ' + volume_value
+
 
 if __name__ == '__main__':
     app.run(debug=True)
