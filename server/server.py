907551a5865449e15cee41bf1f8435151e6b86c6
Akshay Deshraj <akshay30nov@gmail.com>
2015-02-07 09:40:04 +0530

    Add required function definitions

--- server/server.py
+++ server/server.py
@@ -3,7 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def api_root():
-    return 'Welcome'
+	return 'Welcome'
+
+@app.route('/toggle')
+def api_toggle():
+   # Toggle the state of the player
+   return 'state changed'    
+
+@app.route('/volume/<volume_value>')
+def api_volume(volume_value):
+	# Adjusts volume of the player
+	return 'Volume is now ' + volume_value
 
 if __name__ == '__main__':
     app.run()
